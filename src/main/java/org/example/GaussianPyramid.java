d2c74e24fe5897202da83b69dabcee16bd7fc2e0
Jihy <Jihy@yemast.com>
2025-03-14 09:39:34 +0800

    Initial commit

---
+++ src/main/java/org/example/GaussianPyramid.java
@@ -0,0 +1,9 @@
+package org.example;/**
+ *ClassName:GaussianPyramid
+ *Package: org.example  
+ *Description:
+ *@Author:Hanchenk
+ *@Create:2025/3/14 - 9:32
+ *@Version: v1.0 
+*/public class GaussianPyramid {
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * ClassName:${NAME}
+ * Package: org.example
+ * Description:
+ *
+ * @Author:Hanchenk
+ * @Create:2025/3/14 - 9:32
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
